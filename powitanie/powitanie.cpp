b5f3b53f8724cd44348c78683028bbad0b8e939d
Andrzej Ignaczak <a.ignaczak85@gmail.com>
2022-12-23 15:26:34 +0100

    practice with type of data and string

--- powitanie/powitanie.cpp
+++ powitanie/powitanie.cpp
@@ -1,14 +1,23 @@
 //program powitanie do kursu cpp
-/*
-komentarz strefowy
-*/
+
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	cout << "witaj w kursie C++"<<endl;
+	int nrKursu = 123;
+	double cenaKursu = 99.99;
+	char typKursu = 'B';
+	string imie;
+	cout << "Podaj swoje imie: ";
+	cin >> imie;
+	cout << "Witaj " << imie << " w kursie C++!" << endl;
+	cout << "Numer kursu : " << nrKursu << endl;
+	cout << "Cena kursu : " << cenaKursu << "zl" << endl;
+	cout << "Typ kursu : " << typKursu << endl;
+
 	system("pause");
 	return 0;
 } 
